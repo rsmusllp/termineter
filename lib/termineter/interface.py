8d359c95ca47ec10751a60c3a0db2242623eaa87
Spencer McIntyre <zeroSteiner@gmail.com>
2017-12-02 19:32:46 -0500

    Add a new cmd replacement module

--- lib/termineter/interface.py
+++ lib/termineter/interface.py
@@ -20,7 +20,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import cmd
 import code
 import logging
 import os
@@ -33,6 +32,7 @@ import sys
 import textwrap
 
 import termineter
+import termineter.cmd
 import termineter.core
 import termineter.errors
 import termineter.its
@@ -59,75 +59,16 @@ def complete_path(path, allow_files=False):
 		paths = [p for p in paths if not os.path.isfile(p)]
 	return paths
 
-class OverrideCmd(cmd.Cmd, object):
-	__doc__ = 'OverrideCmd class is meant to override methods from cmd.Cmd so they can\nbe imported into the base interpreter class.'
-	def __init__(self, *args, **kwargs):
-		cmd.Cmd.__init__(self, *args, **kwargs)
-
-		self._hidden_commands = ['EOF']
-		self._disabled_commands = []
-		self.__package__ = '.'.join(self.__module__.split('.')[:-1])
-
-	def cmdloop(self):
-		while True:
-			try:
-				super(OverrideCmd, self).cmdloop()
-				return
-			except KeyboardInterrupt:
-				self.print_line('')
-				self.print_error('Please use the \'exit\' command to quit')
-				continue
-
-	def get_names(self):
-		commands = super(OverrideCmd, self).get_names()
-		for name in self._hidden_commands:
-			if 'do_' + name in commands:
-				commands.remove('do_' + name)
-		for name in self._disabled_commands:
-			if 'do_' + name in commands:
-				commands.remove('do_' + name)
-		return commands
-
-	def emptyline(self):
-		# don't do anything on a blank line being passed
-		pass
-
-	def help_help(self):  # Get help out of the undocumented section, stupid python
-		self.do_help('')
-
-	def precmd(self, line):  # use this to allow using '?' after the command for help
-		tmp_line = line.split()
-		if not tmp_line:
-			return line
-		if tmp_line[0] in self._disabled_commands:
-			self.default(tmp_line[0])
-			return ''
-		if len(tmp_line) == 1:
-			return line
-		if tmp_line[1] == '?':
-			self.do_help(tmp_line[0])
-			return ''
-		return line
-
-	def do_exit(self, args):
-		return True
-
-	def do_EOF(self, args):
-		"""Exit The Interpreter"""
-		self.print_line('')
-		return self.do_exit('')
-
 # the core interpreter for the console
-class InteractiveInterpreter(OverrideCmd):
-	__doc__ = 'The core interpreter for the program'
+class InteractiveInterpreter(termineter.cmd.Cmd):
+	"""The core interpreter for the CLI interface."""
 	__name__ = 'termineter'
 	prompt = __name__ + ' > '
 	ruler = '+'
 	doc_header = 'Type help <command> For Information\nList Of Available Commands:'
 	def __init__(self, check_rc_file=True, stdin=None, stdout=None, log_handler=None):
-		OverrideCmd.__init__(self, stdin=stdin, stdout=stdout)
-		if stdin is not None:
-			self.use_rawinput = False
+		super(InteractiveInterpreter, self).__init__(stdin=stdin, stdout=stdout)
+		if not self.use_rawinput:
 			# No 'use_rawinput' will cause problems with the ipy command so disable it for now
 			self._disabled_commands.append('ipy')
 
@@ -210,59 +151,12 @@ class InteractiveInterpreter(OverrideCmd):
 			return False
 		return True
 
-	@staticmethod
-	def serve(addr, run_once=False, log_level=None, use_ssl=False, ssl_cert=None):
-		import ssl
-		__package__ = '.'.join(InteractiveInterpreter.__module__.split('.')[:-1])
-		logger = logging.getLogger(__package__ + '.interpreter.server')
-
-		srv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		srv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		srv_sock.bind(addr)
-		logger.debug('listening for connections on: ' + addr[0] + ':' + str(addr[1]))
-		srv_sock.listen(1)
-		while True:
-			try:
-				(clt_sock, clt_addr) = srv_sock.accept()
-			except KeyboardInterrupt:
-				break
-			logger.info('received connection from: ' + clt_addr[0] + ':' + str(clt_addr[1]))
-
-			if use_ssl:
-				ssl_sock = ssl.wrap_socket(clt_sock, server_side=True, certfile=ssl_cert)
-				ins = ssl_sock.makefile('r', 1)
-				outs = ssl_sock.makefile('w', 1)
-			else:
-				ins = clt_sock.makefile('r', 1)
-				outs = clt_sock.makefile('w', 1)
-
-			log_stream = logging.StreamHandler(outs)
-			if log_level is not None:
-				log_stream.setLevel(log_level)
-			log_stream.setFormatter(logging.Formatter("%(levelname)-8s %(message)s"))
-			logging.getLogger('').addHandler(log_stream)
-
-			interpreter = InteractiveInterpreter(check_rc_file=False, stdin=ins, stdout=outs)
-			try:
-				interpreter.cmdloop()
-			except socket.error:
-				log_stream.close()
-				logging.getLogger('').removeHandler(log_stream)
-				logger.warning('received a socket error during the main interpreter loop')
-				continue
-			log_stream.flush()
-			log_stream.close()
-			logging.getLogger('').removeHandler(log_stream)
-
-			outs.close()
-			ins.close()
-			clt_sock.shutdown(socket.SHUT_RDWR)
-			clt_sock.close()
-			del clt_sock
-			if run_once:
-				break
-		srv_sock.shutdown(socket.SHUT_RDWR)
-		srv_sock.close()
+	@classmethod
+	def serve(cls, *args, **kwargs):
+		init_kwargs = kwargs.pop('init_kwargs', {})
+		init_kwargs['check_rc_file'] = False
+		kwargs['init_kwargs'] = init_kwargs
+		super(InteractiveInterpreter, cls).serve(*args, **kwargs)
 
 	def do_back(self, args):
 		"""Stop using a module"""
